e55292ccf5e618e3ae660877e5a9f8951bc57e88
DoniMassimo <donimassimo3@gmail.com>
2026-04-16 19:16:21 +0200

    refactor(endpoint): refactoring api endpoint

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import fire
+from routes import data
 from flask import Flask, abort, jsonify, request
 from werkzeug.exceptions import HTTPException
 import dotenv
@@ -97,26 +98,6 @@ def start_transl_job(track_id: str):
     return (ret, 202)
 
 
-@app.route("/translations/<string:transl_id>", methods=["DELETE"])
-def delete_translation(transl_id: str):
-    if not fire.transl_exist(transl_id):
-        abort(404, description=f"translation with id {transl_id} does not exisit")
-    fire.delete_translation(transl_id)
-    ret = jsonify({"succes": True, "data": {"transl_id": transl_id}})
-    return (ret, 200)
-
-
-@app.route("/translations_ids")
-def get_translations_ids():
-    transls_ids = fire.get_all_ids()
-    return jsonify(transls_ids), 200
-
-
-@app.route("/translations/<string:transl_id>")
-def get_translations(transl_id: str):
-    return fire.get_transl_data(transl_id)
-
-
 @app.errorhandler(HTTPException)
 def handle_http_exception(error):
     response = {
@@ -136,4 +117,5 @@ def home():
 
 
 if __name__ == "__main__":
+    app.register_blueprint(data.data_api)
     app.run(host="localhost", port=5000, debug=True)
---
+++ routes/data.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, abort, jsonify
+import fire
+
+data_api = Blueprint("api", __name__)
+
+
+@data_api.route("/translations/<string:transl_id>", methods=["DELETE"])
+def delete_translation(transl_id: str):
+    if not fire.transl_exist(transl_id):
+        abort(404, description=f"translation with id {transl_id} does not exisit")
+    fire.delete_translation(transl_id)
+    ret = jsonify({"succes": True, "data": {"transl_id": transl_id}})
+    return (ret, 200)
+
+
+@data_api.route("/translations_ids")
+def get_translations_ids():
+    transls_ids = fire.get_all_ids()
+    return jsonify(transls_ids), 200
+
+
+@data_api.route("/translations/<string:transl_id>")
+def get_translations(transl_id: str):
+    return fire.get_transl_data(transl_id)
