62fa8dcbf4e8cb498a4eb0111719cdfd4cdb0967
DoniMassimo <donimassimo3@gmail.com>
2026-04-16 19:13:45 +0200

    chore(test): removed unused test

--- test/core.py
+++ test/core.py
@@ -11,15 +11,8 @@ import spoty
 import lyrics_mod
 import fire
 
-lyrics_mod.lyrics_mod_init()
 spoty.spoty_init()
 fire.fire_init()
-
-
-def generate_download_lyric_mock():
-    lyric = lyrics_mod.download_lyrics_syrics("4sJFW6APhXl5v7idGF3QWd")
-    with open("mock_file/mock_lyric.json", "w", encoding="utf-8") as file:
-        json.dump(lyric, file, ensure_ascii=False, indent=4)
 
 
 def generate_spoty_track_info_mock():
